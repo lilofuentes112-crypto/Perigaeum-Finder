85a7160fc29b91865e3720d83b092799fe054044
lilofuentes112-crypto <lilo.fuentes112@gmail.com>
2025-11-26 16:56:04 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-codeTypeScript
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Füge diese Zeile hinzu, um sicherzustellen, 
+  // dass alle Assets relativ vom aktuellen Ordner geladen werden
+  base: './', 
 })
 
