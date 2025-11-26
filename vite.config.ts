d65076fc1c837670ed5224afa2ab2d7163ebefaf
lilofuentes112-crypto <lilo.fuentes112@gmail.com>
2025-11-25 23:02:51 -0600

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+codeTypeScript
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+
