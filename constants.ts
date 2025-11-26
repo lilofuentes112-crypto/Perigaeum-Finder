44effa4fe39dcf7fc2dcfaa34d7591591539a8b5
lilofuentes112-crypto <lilo.fuentes112@gmail.com>
2025-11-25 23:14:47 -0600

    Create constants.ts

---
+++ constants.ts
@@ -0,0 +1,15 @@
+export const PLANET_COLORS: Record<string, string> = {
+  Merkur: "from-gray-300 to-gray-500",
+  Venus: "from-yellow-200 to-orange-300",
+  Mars: "from-red-400 to-red-600",
+  Jupiter: "from-orange-100 to-orange-400",
+  Saturn: "from-yellow-100 to-yellow-600",
+  Chiron: "from-teal-200 to-teal-500",
+  Uranus: "from-cyan-300 to-blue-500",
+  Neptun: "from-blue-500 to-indigo-700",
+  Pluto: "from-stone-300 to-stone-500",
+};
+
+export const MIN_YEAR = 1900;
+export const MAX_YEAR = 2050;
+
