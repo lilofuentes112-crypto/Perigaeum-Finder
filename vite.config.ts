94f1daba13ac40a745aa97ed80a9f7e02feb5a72
lilofuentes112-crypto <lilo.fuentes112@gmail.com>
2025-11-26 18:26:47 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Korrektur für Vercel/GitHub Pages: 
+  // Stellt sicher, dass alle Assets relativ vom aktuellen Ordner geladen werden, 
+  // um den Rollup-Fehler zu beheben, wenn der Pfad in index.html auf ./src/... gesetzt ist.
+  base: './', 
 })
 
